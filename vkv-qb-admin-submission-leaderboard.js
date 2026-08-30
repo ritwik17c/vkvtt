c8dca6ac9d4d44f81e20a2281bf486ce235c147d
ritwik17c <69358070+ritwik17c@users.noreply.github.com>
2026-08-30 19:44:38 +0530

    QB: load Principal pending-age analytics

--- vkv-qb-admin-submission-leaderboard.js
+++ vkv-qb-admin-submission-leaderboard.js
@@ -1,3 +1,4 @@
+import './vkv-qb-admin-pending-age.js?v=20260830-1';
 import{getApps,getApp}from'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import{getFirestore,collection,query,where,getCountFromServer}from'https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js';
 const wait=ms=>new Promise(r=>setTimeout(r,ms));
