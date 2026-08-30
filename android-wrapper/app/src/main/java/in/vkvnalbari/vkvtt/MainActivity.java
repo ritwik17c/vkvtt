06c40c2cbff7669fa5b5fba65d6c7bca8f79ba21
ritwik17c <69358070+ritwik17c@users.noreply.github.com>
2026-08-30 14:17:58 +0530

    Open VKVTT in Custom Tab for supported Google authentication

--- android-wrapper/app/src/main/java/in/vkvnalbari/vkvtt/MainActivity.java
+++ android-wrapper/app/src/main/java/in/vkvnalbari/vkvtt/MainActivity.java
@@ -1,48 +1,22 @@
 package in.vkvnalbari.vkvtt;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
+import androidx.browser.customtabs.CustomTabsIntent;
 
 public class MainActivity extends Activity {
-    private WebView webView;
     private static final String HOME = "https://ritwik17c.github.io/vkvtt/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        webView = new WebView(this);
-        setContentView(webView);
 
-        WebSettings s = webView.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setDatabaseEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(true);
-        s.setMediaPlaybackRequiresUserGesture(false);
-        s.setBuiltInZoomControls(false);
-        s.setDisplayZoomControls(false);
-
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
-
-        if (savedInstanceState == null) webView.loadUrl(HOME);
-        else webView.restoreState(savedInstanceState);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        webView.saveState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .build();
+        customTabsIntent.launchUrl(this, Uri.parse(HOME));
+        finish();
     }
 }
